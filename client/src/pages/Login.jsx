b061ed2ead6f737c0fd4d4fda3f627f967423a30
Utkarsh <utkarshparihar920@gmail.com>
2026-08-19 17:47:40 +0530

    Use API environment variable for login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     try {
 
       const response = await fetch(
-        "http://localhost:5000/api/auth/login",
+        `${import.meta.env.VITE_API_URL}/api/auth/login`,
         {
           method: "POST",
 
